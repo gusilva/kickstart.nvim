e1d750c09f43d2c73fd8813346d353145d85217d
Gustavo Silva <silvagustavo@yandex.ru>
2023-10-25 21:56:32 +0200

    add dadbod

--- init.lua
+++ init.lua
@@ -148,6 +148,15 @@ require('lazy').setup({
         end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
       end,
     },
+    config = function()
+      require('gitsigns').setup {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 1000,
+          virt_text_pos = 'eol',
+        },
+      }
+    end,
   },
 
   {
