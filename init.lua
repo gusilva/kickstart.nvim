b6ae6c7a0e51a70e53134ac5394e3e270b8573a8
Gustavo Silva (es00679121_prosegur) <gustavo.neves@prosegur.com>
2024-01-21 20:35:43 +0100

    add chatgpt, norg and gen plugins

--- init.lua
+++ init.lua
@@ -575,11 +575,11 @@ local servers = {
     }
   },
   grammarly = {
-    filetypes = { 'markdown' },
+    filetypes = { 'markdown', 'norg' },
   },
+  tsserver = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
